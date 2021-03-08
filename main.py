229f70c2ce980b3c1051efe7033d6f3bece3e620
Oscar Chinellato <oscar.chinellato@sickevelopers.com>
2021-03-08 17:20:24 +0100

    update socket

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import socket
 
 print("Long running script start")
 
-time.sleep(5)
+time.sleep(2)
 
 client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
 client_socket.connect('/tmp/inference-test')
-client_socket.send("SENT FROM PYTHON")
+client_socket.send("SENT FROM PYTHON".encode())
 
-time.sleep(5)
+time.sleep(2)
 
 print("end long running script")
 
