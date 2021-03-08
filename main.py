220e969cdbf5b0fea00a63c2665e70fce9485936
Oscar Chinellato <oscar.chinellato@sickevelopers.com>
2021-03-08 16:55:29 +0100

    update socket

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import time
+import os
+import socket
 
 print("Long running script start")
-time.sleep(10)
+
+time.sleep(5)
+
+client_socket = socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM)
+client_socket.connect(os.environ.get('SOCKET_PATH'))
+client_socket.send("SENT FROM PYTHON")
+
+time.sleep(5)
+
 print("end long running script")
 
